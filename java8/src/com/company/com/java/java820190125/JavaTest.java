ff3de284c0a2525a7af0c993ec07db276bef1969
Vincent <445047477@qq.com>
2019-01-30 11:12:13 +0800

    add some

--- java8/src/com/company/com/java/java820190125/JavaTest.java
+++ java8/src/com/company/com/java/java820190125/JavaTest.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeParseException;
 
 public class JavaTest {
     public static void main(String[] args) {
+        //add some for test
         String goodFriday = "Apr 18 2014";
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
